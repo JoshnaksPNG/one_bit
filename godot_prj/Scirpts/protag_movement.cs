c18e1e5cd9486128311808a1c0c8a5ef53ed0bab
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2023-11-13 12:15:25 -0800

    Dash Control Added
    Added Dash Movement to protag_movement.
    
    Moved Door Trigger in Village Level

--- godot_prj/Scirpts/protag_movement.cs
+++ godot_prj/Scirpts/protag_movement.cs
@@ -6,11 +6,19 @@ public partial class protag_movement : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -900.0f;
+    public const float DashVelocity = 900f;
 
 
 	// Custom Movement Variables
 	private bool hasDoubleJump = false;
 	private const float doubleJumpRatio = (2f / 3f);
+
+    const double DASH_DURATION = 0.3d;
+    double dash_timer = DASH_DURATION;
+    bool is_dashing = false;
+    int dash_direction = 0;
+    bool can_dash = true;
+
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -34,69 +42,110 @@ public partial class protag_movement : CharacterBody2D
         if (!lvl_manager.is_changing_level) // Controll Normally if Level is Not Changing
         {
             Vector2 velocity = Velocity;
-            Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
-            // Add the gravity and animations
-            if (!IsOnFloor())
+            if (!is_dashing)
             {
-                velocity.Y += gravity * (float)delta;
-                animatedSprite2D.Play("jump");
-            }
-            else
-            {
-                // Add Double Jump Back If Touching Ground
-                if (!hasDoubleJump)
-                {
-                    hasDoubleJump = true;
-                }
+                
+                Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
-
-                if (velocity != Vector2.Zero)
+                // Add the gravity and animations
+                if (!IsOnFloor())
                 {
-                    animatedSprite2D.Play("walk");
-                    if (direction.X > 0)
-                    {
-                        animatedSprite2D.FlipH = false;
-                    }
-                    else
-                    {
-                        animatedSprite2D.FlipH = true;
-                    }
+                    velocity.Y += gravity * (float)delta;
+                    animatedSprite2D.Play("jump");
                 }
                 else
                 {
-                    animatedSprite2D.Play("default");
+                    // Add Double Jump and Dash Back If Touching Ground
+                    hasDoubleJump = true;
+                    can_dash = true;
+
+                    if (velocity != Vector2.Zero)
+                    {
+                        animatedSprite2D.Play("walk");
+                        if (direction.X > 0)
+                        {
+                            animatedSprite2D.FlipH = false;
+                        }
+                        else
+                        {
+                            animatedSprite2D.FlipH = true;
+                        }
+                    }
+                    else
+                    {
+                        animatedSprite2D.Play("default");
+                    }
                 }
-            }
 
 
-            // Handle Jump.
-            if (Input.IsActionJustPressed("character_jump"))
-            {
-                if (IsOnFloor())
+                // Handle Jump.
+                if (Input.IsActionJustPressed("character_jump"))
                 {
-                    velocity.Y = JumpVelocity;
+                    if (IsOnFloor())
+                    {
+                        velocity.Y = JumpVelocity;
+                    }
+                    else if (hasDoubleJump)
+                    {
+                        hasDoubleJump = false;
+
+                        float doubleJumpVelocity = JumpVelocity * doubleJumpRatio;
+
+                        velocity.Y = doubleJumpVelocity < velocity.Y ? doubleJumpVelocity : velocity.Y;
+                    }
                 }
-                else if (hasDoubleJump)
+
+
+                // Get the input direction and handle the movement/deceleration.
+                // As good practice, you should replace UI actions with custom gameplay actions.
+                if (direction != Vector2.Zero)
                 {
-                    hasDoubleJump = false;
-
-                    float doubleJumpVelocity = JumpVelocity * doubleJumpRatio;
-
-                    velocity.Y = doubleJumpVelocity < velocity.Y ? doubleJumpVelocity : velocity.Y;
+                    velocity.X = direction.X * Speed;
                 }
-            }
+                else
+                {
+                    velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+                }
 
+                // Handle Dashing
+                if (can_dash)
+                {
+                    bool movingLeft = direction.X < 0;
 
-            // Get the input direction and handle the movement/deceleration.
-            // As good practice, you should replace UI actions with custom gameplay actions.
-            if (direction != Vector2.Zero)
-            {
-                velocity.X = direction.X * Speed;
+                    if (Input.IsActionJustPressed("dash_left") || (Input.IsActionJustPressed("dash") && movingLeft))
+                    {
+                        dash_direction = -1;
+                        is_dashing = true;
+                        can_dash = false;
+                    }
+                    else if (Input.IsActionJustPressed("dash_right") || (Input.IsActionJustPressed("dash") && !movingLeft))
+                    {
+                        dash_direction = 1;
+                        is_dashing = true;
+                        can_dash = false;
+                    }
+                    else
+                    {
+                        dash_direction = 0;
+                        dash_timer = DASH_DURATION;
+                    }
+                }
+                
             }
-            else
+            else // If Character is Dashing
             {
-                velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+                if (dash_timer <= 0)
+                {
+                    is_dashing = false;
+                }
+                else
+                {
+                    velocity.X = DashVelocity * dash_direction;
+                    velocity.Y = 0;
+
+                    dash_timer -= delta;
+                }
             }
 
             Velocity = velocity;
