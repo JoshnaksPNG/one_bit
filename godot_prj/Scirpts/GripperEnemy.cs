c9a2ab1224073ed7383fa0f06492c72d9ae6c3cb
MooseTheMoosieMoose <85206403+MooseTheMoosieMoose@users.noreply.github.com>
2023-11-16 09:17:33 -0800

    Reapplied protag collisons

--- godot_prj/Scirpts/GripperEnemy.cs
+++ godot_prj/Scirpts/GripperEnemy.cs
@@ -4,15 +4,22 @@ using System;
 public partial class GripperEnemy : RigidBody2D
 {
 	[Export]
-	private Vector2 mov = new Vector2(1, -2);
+	private Vector2 speed = new Vector2(0, 0);
+	private CharacterBody2D player;
+	private Vector2 target_pos;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		player = GetNode<CharacterBody2D>("/root/GameRuntime/ProtagBody");
+		target_pos = player.Position;
 	}
 	public override void _IntegrateForces(PhysicsDirectBodyState2D state)
 	{
-		state.AddConstantForce(mov);
+		// Vector2 mov = new Vector2();
+		// mov.X = Mathf.MoveToward(Position.X, target_pos.X, speed.X);
+		// mov.Y = Mathf.MoveToward(Position.Y, target_pos.Y, speed.Y);
+		// state.AddConstantForce(mov);
+		// target_pos = player.Position;
 	}
 }
